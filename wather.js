f51c0ff7484d3e7ebd4657cbd310a99856024401
umar azeem <125313441+Umar-azeem@users.noreply.github.com>
2025-02-24 13:05:05 +0500

    Update wather.js

--- wather.js
+++ wather.js
@@ -1,3 +1,4 @@
+
 const apiKey ="792c3d464a0cc74abde03d9f4c375f1e";
 const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 const searchBox = document.querySelector(".search input");
